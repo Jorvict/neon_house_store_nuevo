ffb8e5afbe69ebd92817212fc3818584882ef9b6
Jorvict Piña <jorvict15@gmail.com>
2021-01-31 23:13:12 -0500

    Se establecen submenus vista móvil

--- public/js/menu.js
+++ public/js/menu.js
@@ -11,5 +11,33 @@ function ShowHideMenu(){
         menu.classList.add('is-active');
     }
 }
-
 menuButton.addEventListener('click', ShowHideMenu);
+
+// Desplegar submenu 1
+const displayfirstbutton = document.querySelector('.fas.first');
+console.log(displayfirstbutton);
+const firstoption = document.querySelector('.hidden-desk.first');
+
+function ShowHideFirst(){
+    if(firstoption.classList.contains('is-active')){
+        firstoption.classList.remove('is-active');
+    }
+    else{
+        firstoption.classList.add('is-active');
+    }
+}
+displayfirstbutton.addEventListener('click', ShowHideFirst);
+
+// Desplegar submenu 2
+const displaysecondbutton = document.querySelector('.fas.second');
+const secondoption = document.querySelector('.hidden-desk.second');
+
+function ShowHideSecond(){
+    if(secondoption.classList.contains('is-active')){
+        secondoption.classList.remove('is-active');
+    }
+    else{
+        secondoption.classList.add('is-active');
+    }
+}
+displaysecondbutton.addEventListener('click', ShowHideSecond);
